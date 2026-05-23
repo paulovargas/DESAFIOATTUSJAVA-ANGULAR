7fdb97f25094cfabd0626c0d1d3933874f00cf87
Paulo Vargas <paulotomegomesdevargas@gmail.com>
2026-05-23 10:42:52 -0300

    Inicialização do projeto

---
+++ java-web-spring-api-attus/src/main/java/com/attus/debt_recovery/DebtRecoverySystemApplication.java
@@ -0,0 +1,13 @@
+package com.attus.debt_recovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebtRecoverySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebtRecoverySystemApplication.class, args);
+    }
+
+}
